9a0044a21e6beeb15ef98c5bcf1b856604d3e1d7
eugabimariA <124887848+eugabimariA@users.noreply.github.com>
2023-08-31 09:12:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+tempo = 5000;
+const idade = 18; 
+age = 18;
+age = age + 1;
+var timer =setInterval(chamar,tempo);
+
+chamar();
+function chamar(){
+//document.getElement só é chamado com tempo
+document.getElementById("mostrar").innerHTML = idade;
+document.getElementById("mostrar").innerHTML = age;
+}
